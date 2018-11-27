e515a73acc50c19852070e54e408ba42ac56cd03
Lionel Dcosta <lionel.dcosta@impelsys.com>
2018-11-27 18:43:47 +0530

    Renamed Pages to screen

--- src/universal/web/screen/index-page/index.js
+++ src/universal/web/screen/index-page/index.js
@@ -1,5 +1,5 @@
 import Loadable from 'react-loadable';
-import LoadingPage from 'universal/web/pages/loading-page';
+import LoadingPage from 'universal/web/screen/loading-page';
 import './style.scss';
 
 export default Loadable({
--- src/universal/web/screen/not-found-page/not-found-error.js
+++ src/universal/web/screen/not-found-page/not-found-error.js
@@ -1,23 +1,29 @@
 /* eslint "react/prop-types": 0*/
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import Loadable from 'react-loadable';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 import { getPageTitle } from './../../../services/page/actionSaga';
 import { getPageTitleAction } from './../../../services/page/actionCreator';
-import LoadingPage from 'universal/web/pages/loading-page';
 
+import Loadable from 'react-loadable';
+import LoadingPage from 'universal/web/screen/loading-page';
 
-class NotFound extends Component {
+const Title =  Loadable({
+  loader: () => import('./title'),
+  loading: LoadingPage,
+});
+
+class NotFoundError extends Component {
   
-  static preLoad = () => [[getPageTitle, 'Not Found Page']];
+  static preLoad = () => [[getPageTitle, 'Not Found Page error']];
 
   constructor(props) {
     super(props);
   }
 
   componentDidMount() {
+
     //this.props.dispatch(getPageTitleAction('NOT FOUND'));
   }
 
@@ -27,8 +33,11 @@ class NotFound extends Component {
         <Helmet>
           <title>{this.props.page_title}</title>
         </Helmet>
-        <Link to="/not-found/1" href="Home Page">
-          <p>Go to Not found error page</p>
+
+        <h1>Not found error page</h1>
+        <Title />
+        <Link to="/not-found" href="Home Page">
+          <p>Go to Not found page</p>
         </Link>
       </section>
     );
@@ -40,4 +49,4 @@ function mapStateToProps(state) {
     page_title: state.Page.pageTitle,
   };
 }
-export default connect(mapStateToProps)(NotFound);
+export default connect(mapStateToProps)(NotFoundError);
