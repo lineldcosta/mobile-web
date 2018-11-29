12af1e1cd85a4aa1bf5be8c8df8d41a995c7f8e2
Lionel Dcosta <lionel.dcosta@impelsys.com>
2018-11-29 19:23:21 +0530

    Configuring react-native to web

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ import App from './src/device/entry/index';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+/*AppRegistry.runApplication(appName, { rootTag: document.getElementById('react-app') });
+*/
